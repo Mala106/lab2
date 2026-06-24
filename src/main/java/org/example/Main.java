96143a0d1c5bafa807e87f4f11e3f853835f6082
Mala <malakc106@gmail.com>
2026-06-25 00:33:07 +0530

    Migrated Maven project to Gradle

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello students");
+        System.out.println("Welcome to Devops Lab");
+        System.out.println("Lab Program no.4");
+    }
+}
